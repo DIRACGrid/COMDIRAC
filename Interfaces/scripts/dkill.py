81a705e4e54fc47f6753286e73a51bad941ec86c
Pierre Gay <pierre.gay@u-bordeaux1.fr>
2013-04-24 22:53:18 +0200

    add option to kill all user's jobs

--- Interfaces/scripts/dkill.py
+++ Interfaces/scripts/dkill.py
@@ -6,7 +6,8 @@ __RCSID__ = "$Id$"
 
 import DIRAC
 from DIRAC.Core.Base import Script
-#
+from COMDIRAC.Interfaces import DSession
+
 class Params:
   def __init__ ( self ):
     self.delete = False
@@ -17,6 +18,12 @@ class Params:
 
   def getDelete( self ):
     return self.delete
+
+  def setSelectAll( self, arg = None ):
+    self.selectAll = True
+
+  def getSelectAll( self ):
+    return self.selectAll
 
   def setVerbose( self, arg = None ):
     self.verbose = True
@@ -32,6 +39,7 @@ Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                      'Arguments:',
                                      '  JobID: a DIRAC job identifier', ] ) )
 Script.registerSwitch( "D", "delete", "delete job", params.setDelete )
+Script.registerSwitch( "a", "all", "select all jobs", params.setSelectAll )
 Script.registerSwitch( "v", "verbose", "verbose output", params.setVerbose )
 
 Script.parseCommandLine( ignoreErrors = True )
@@ -40,10 +48,27 @@ args = Script.getPositionalArgs()
 exitCode = 0
 
 from DIRAC.WorkloadManagementSystem.Client.WMSClient import WMSClient
+from DIRAC.Core.DISET.RPCClient import RPCClient
 
 wmsClient = WMSClient()
 
 jobs = args
+
+if params.getSelectAll():
+  session = DSession()
+  Script.enableCS()
+  result = session.getUserName()
+  if result["OK"]:
+    userName = result["Value"]
+
+    monitoring = RPCClient( 'WorkloadManagement/JobMonitoring' )
+    result = monitoring.getJobs( {"Owner" : userName} )
+    if not result['OK']:
+      print "ERROR:", result['Message']
+    else:
+      jobs += map ( int, result['Value'] )
+  else:
+    print "ERROR:", result["Message"]
 
 errors = []
 for job in jobs:
