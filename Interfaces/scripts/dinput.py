72051fba06a389a3ff6a5086ae65769061cedaf5
Pierre Gay <pierre.gay@u-bordeaux1.fr>
2013-12-11 23:22:54 +0100

    don't download inpus sandbox when JDL is required

--- Interfaces/scripts/dinput.py
+++ Interfaces/scripts/dinput.py
@@ -16,6 +16,7 @@ class Params:
     self.outputDir = None
     self.verbose = False
     self.downloadJDL = False
+    self.inputSandbox = False
 
   def setOutputDir( self, arg = None ):
     self.outputDir = arg
@@ -28,6 +29,12 @@ class Params:
 
   def getDownloadJDL( self ):
     return self.downloadJDL
+
+  def setInputSandbox( self, arg = None ):
+    self.inputSandbox = True
+
+  def getInputSandbox( self ):
+    return self.inputSandbox
 
   def setVerbose( self, arg = None ):
     self.verbose = True
@@ -45,7 +52,8 @@ Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                      '  JobID:    DIRAC Job ID' ] ) )
 
 Script.registerSwitch( "D:", "OutputDir=", "destination directory", params.setOutputDir )
-Script.registerSwitch( "j", "JDL", "also download job JDL", params.setDownloadJDL )
+Script.registerSwitch( "j", "JDL", "download job JDL instead of input sandbox", params.setDownloadJDL )
+Script.registerSwitch( "", "Sandbox", "donwload input sandbox, even if JDL was required", params.setInputSandbox )
 Script.registerSwitch( "v", "verbose", "verbose output", params.setVerbose )
 
 Script.parseCommandLine( ignoreErrors = True )
@@ -61,7 +69,7 @@ jobs = []
 outputDir = params.getOutputDir() or os.path.curdir
 
 for arg in args:
-  if os.path.isdir( os.path.join( outputDir, arg ) ):
+  if os.path.isdir( os.path.join( outputDir, "InputSandbox%s" % arg ) ):
     print "Input for job %s already retrieved, remove the output directory to redownload" % arg
   else:
     jobs.append( arg )
@@ -74,13 +82,18 @@ if jobs:
   inputs = {}
   for job in jobs:
     destinationDir = os.path.join( outputDir, "InputSandbox%s" % job )
+    if not os.path.exists( destinationDir ): os.mkdir( destinationDir )
+
     inputs[job] = {"destinationDir" : destinationDir}
-    result = dirac.getInputSandbox( job, outputDir = outputDir )
-    if result['OK']:
-      inputs[job]["isb"] = destinationDir
-    else:
-      errors.append( result["Message"] )
-      exitCode = 2
+
+    if params.getInputSandbox() or not params.getDownloadJDL():
+      result = dirac.getInputSandbox( job, outputDir = outputDir )
+      if result['OK']:
+        inputs[job]["isb"] = destinationDir
+      else:
+        errors.append( result["Message"] )
+        exitCode = 2
+
     if params.getDownloadJDL():
       result = dirac.getJobJDL( job, printOutput = False )
       if result['OK']:
